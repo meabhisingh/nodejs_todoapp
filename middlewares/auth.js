e443afe144086d784797d2739750d23e41dbf4ea
Shishir <90249481+shishirkj@users.noreply.github.com>
2023-11-16 11:53:00 +0530

    Update auth.js
    used errorHandeler in auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,11 +4,8 @@ import jwt from "jsonwebtoken";
 export const isAuthenticated = async (req, res, next) => {
   const { token } = req.cookies;
 
-  if (!token)
-    return res.status(404).json({
-      success: false,
-      message: "Login First",
-    });
+if (!token)
+    return  next(new ErrorHandler("Login First",404))
 
   const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
